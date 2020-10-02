7c7f8a4d431ac2bb9e5d8643745770ade202ed16
utkarsh <utkarshsharmaalua@gmail.com>
2020-10-02 21:54:19 +0530

    Updates

---
+++ precache-manifest.095a9b4c420735215a7db374151b6b8b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a6c42f9d928e3455d861b37ea94a0a4",
+    "url": "/Todo/index.html"
+  },
+  {
+    "revision": "e37208d9a6fe7d5d21c4",
+    "url": "/Todo/static/css/2.41ce4b5e.chunk.css"
+  },
+  {
+    "revision": "905050fea9cb6687c08c",
+    "url": "/Todo/static/css/main.7147e0e7.chunk.css"
+  },
+  {
+    "revision": "e37208d9a6fe7d5d21c4",
+    "url": "/Todo/static/js/2.4e62d388.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo/static/js/2.4e62d388.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "905050fea9cb6687c08c",
+    "url": "/Todo/static/js/main.a33b18ec.chunk.js"
+  },
+  {
+    "revision": "162a65c0456d03b31aa2",
+    "url": "/Todo/static/js/runtime-main.e3579041.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Todo/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Todo/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Todo/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Todo/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Todo/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
